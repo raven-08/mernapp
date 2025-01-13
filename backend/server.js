ff5ccadaf5c198e312e709785902925a393f9f2a
sir_lancaster <135249298+raven-08@users.noreply.github.com>
2025-01-13 13:40:24 +0800

    Final blow

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const path = require('path');
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const { errorHandler } = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorHandler");
+const notFound = require("./middleware/notFound");
+const connectDB = require("./config/db");
+const logger = require("./middleware/logger");
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -13,22 +15,26 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
+if (process.env.NODE_ENV == "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+	);
 }
-
+else{
+  app.get('/', (req, res) => res.send('Please set to production'))
+}
+// Error Handlers
 app.use(errorHandler);
+app.use(notFound);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Logger middleware
+app.use(logger);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
