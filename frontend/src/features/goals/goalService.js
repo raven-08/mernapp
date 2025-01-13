c3e09f21f2db1b0b1990a232384995c6bd70084a
sir_lancaster <135249298+raven-08@users.noreply.github.com>
2025-01-13 14:31:04 +0800

    Update goalService.js

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://mernapp-server-0zza.onrender.com";
+const API_URL = `${process.env.REACT_APP_API_URL || "http://localhost:5000"}/api/goals/`;
+
 
 // Create new goal
 const createGoal = async (goalData, token) => {
