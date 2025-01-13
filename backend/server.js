5c237221438aee8e5d73fc220ce04f57e1e3b8b5
sir_lancaster <135249298+raven-08@users.noreply.github.com>
2025-01-13 18:46:59 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const cors = require("cors"); // Import cors module
 const { errorHandler } = require("./middleware/errorHandler");
 const notFound = require("./middleware/notFound");
 const connectDB = require("./config/db");
@@ -15,29 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", require("./routes/goalRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-
-// Serve frontend
-
-if (process.env.NODE_ENV == "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-	app.get("*", (req, res) =>
-		res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
-	);
-}
-else{
-  app.get('/', (req, res) => res.send('Please set to production'))
-}
-
-
-// Error Handlers
-app.use(errorHandler);
-app.use(notFound);
-
-
-
+// Use CORS middleware
 app.use(
 	cors({
 		origin: "https://mernapp-frontend-r92o.onrender.com", // Replace with your frontend URL
@@ -45,5 +24,23 @@ app.use(
 		credentials: true, // If you're using cookies or other credentials
 	})
 );
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+	);
+} else {
+	app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+// Error Handlers
+app.use(errorHandler);
+app.use(notFound);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
